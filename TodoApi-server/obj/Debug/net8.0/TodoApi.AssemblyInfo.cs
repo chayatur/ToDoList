8c29385144a344cc758cd898a007ea915ad29d3d
chaya <c6760083@gmail.com>
2025-02-09 16:55:35 +0200

    finish:)

--- TodoApi-server/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi-server/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+630924d43a5586e22cb209942b705f4cc48a1fad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2fcaab95246c1b9c753fbc74f85ab51ed76b5b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
